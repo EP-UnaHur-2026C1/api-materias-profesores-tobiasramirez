79d00d87020bcba9b4c9828cd60128b3b0e8223c
tobiasramirez <tobi05cabj@gmail.com>
2026-04-27 19:06:17 -0300

    ruta updateMateria agregada

--- routes/materias.route.js
+++ routes/materias.route.js
@@ -1,10 +1,11 @@
 const {Router} = require('express')
-const {getMaterias, getMateriaById, createMateria} = require('../controllers/materias.controller')
+const {getMaterias, getMateriaById, createMateria, updateMateria} = require('../controllers/materias.controller')
 
 const route = Router()
 
 route.get('/materias', getMaterias)
 route.get('/materias/:id', getMateriaById)
 route.post("/materias", createMateria)
+route.put("/materias/:id", updateMateria)
 
 module.exports = route
