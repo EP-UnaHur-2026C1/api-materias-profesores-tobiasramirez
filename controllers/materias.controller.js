a142c86f92d735a5ac51fda21a317d9209884893
tobiasramirez <tobi05cabj@gmail.com>
2026-04-27 19:04:25 -0300

    metodos create y update para Materia

--- controllers/materias.controller.js
+++ controllers/materias.controller.js
@@ -5,9 +5,33 @@ const getMaterias = async (req, res)=>{
   console.log(materias)
     res.status(200).json(materias)
 }
-const getMateriaById = (req, res)=>{
+const getMateriaById = async(req, res)=>{
+    try { const id = await req.params.id
+    res.status(200).json({id})  }
+    catch(e){
+      req.status(400).json({message: "Error"})
+    }
+}
+const createMateria = async(req,res)=>{
+  try {
+    const nuevaMateria =  req.body
+    const materia = await Materia.create(nuevaMateria)
+    res.status(200).json(materia)
+ }catch(e){
+  res.status(500).json({message:"Error"})
+ }
+}
+const updateMateria = async(req,res)=>{
+  try {
     const id = req.params.id
-    res.status(200).json({id})
+    const actualizacion = req.body
+    const materia = await Materia.findByPK(id)
+    await materia.update(actualizacion)
+    res.status(200).json(materia)
+  } catch(e){
+    req.status(400).json({message: "Error"})
+  }
 }
 
-module.exports = {getMaterias, getMateriaById}
+
+module.exports = {getMaterias, getMateriaById, createMateria, updateMateria}
--- routes/materias.route.js
+++ routes/materias.route.js
@@ -1,9 +1,10 @@
 const {Router} = require('express')
-const {getMaterias, getMateriaById} = require('../controllers/materias.controller')
+const {getMaterias, getMateriaById, createMateria} = require('../controllers/materias.controller')
 
 const route = Router()
 
 route.get('/materias', getMaterias)
 route.get('/materias/:id', getMateriaById)
+route.post("/materias", createMateria)
 
 module.exports = route
